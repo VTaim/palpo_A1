478bb855f22787fc10a17f120784c4e3e727a0ea
tenderi <helge.jalonen@iki.fi>
2015-11-03 16:26:50 +0200

    testailenpa paskaa

---
+++ testi2.py
@@ -0,0 +1,5 @@
+
+""" Hei mäkin testaan! Tämän tiedosto vastaa kurssilla MAT-82106 Semantic Modeling ensimmäisen
+assignmentin tehtävänannon osaan A1a. Harjoitustyön ovat toteuttaneet
+Valtteri Taimela ja Helge Jalonen"""
+
