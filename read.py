4f6227cc2d11ab526d16816235a0139dec3f0895
tenderi <helge.jalonen@iki.fi>
2015-11-03 16:39:03 +0200

    dessddesd

--- read.py
+++ read.py
@@ -1,5 +1,5 @@
 
 """Tämän tiedosto vastaa kurssilla MAT-82106 Semantic Modeling ensimmäisen
 assignmentin tehtävänannon osaan A1a. Harjoitustyön ovat toteuttaneet
-Valtteri Taimela ja Helge Jalonen"""
+HOMONAAMA :dDDDd ja Helge Jalonen"""
 
