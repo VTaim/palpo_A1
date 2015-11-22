35e75a5bc62518e38e11fe7f7993fa6dbf334edb
tenderi <helge.jalonen@iki.fi>
2015-11-22 16:43:10 +0200

    Korjattu luentoesimerkkien mukaiseksi

--- elvisA1a.py
+++ elvisA1a.py
@@ -3,16 +3,29 @@ import logging
 from rdflib import RDF, Graph, URIRef
 from rdflib.parser import Parser
 
-# Some data has whitespace in URIs, have to fix that (Source: https://github.com/RDFLib/rdflib/issues/412#issuecomment-50247061)
+def localish(s):
+  """Return the (almost) local name part of a URI,
+  e.g., http:///www.ex.org/foo -> /foo"""
+  a = s.rfind('/'); b = s.rfind('#');
+  if a==-1 and b==-1: return s
+  return s[max(a,b):]
+
+def addIfUnseenRes(r,s):
+  """Add s to r if so far unseen and of proper type (URIRef or BNode)"""
+  if not (type(s) is rdflib.URIRef or type(s) is rdflib.BNode): return
+  if not (s in r): r.append(s)
+
 
 # In order to get error messages straight from rdflib...
 logging.basicConfig()
 
 # Creating the graph with parser
 g = rdflib.Graph()
-g.open("store", create=True)
-result = g.parse("elvisimp.rdf", format='application/rdf+xml')
+result = g.parse("http://www.snee.com/rdf/elvisimp.rdf", format='application/rdf+xml')
 
-# print out all the triples in the graph, A1a
-for subject, predicate, object in g:
-    print subject, predicate, object
+#print everything
+
+for s, p, o in g.triples((None, None, None)):
+  g.add((s,p,o))
+print(g.serialize(format="application/rdf+xml"))
+
