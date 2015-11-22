7d5e056296904b6dda023548658d28c89ee3b3e2
tenderi <helge.jalonen@iki.fi>
2015-11-22 19:52:22 +0200

    Ei vielä valmis....

--- elvisA1c.py
+++ elvisA1c.py
@@ -1,27 +1,32 @@
 # -*- coding: utf-8 -*-
-#A1b: The program must then navigate through all the resources (identified by URIs or Blank nodes) in the model. If the type (rdf:type) of the resource is not known, add an asserted statement to the model (in memory) that declares that the type of the resource is LocallyUnknownType (suggest proper URI etc.). (1 point)
-#For each statement, check which URIRefs and BNodes appear in each statement. For those resources x (i.e. URIRefs and BNodes) for which there is no asserted statement about their type (i.e. something like (x, rdf:type, y)), add a new statement (x, rdf:type : LocallyUnknownType). How to do this in practice, check section 3 and RDFLib examples in Moodle Lecture Material section, or browse RDFLIb documentation.
+#Also, for each resource, print out the resource types (including the types you perhaps asserted), using a simple text format that can be easily uploaded to a spreadsheet editor (e.g. MS Excel or OO Calc; tip: CSV). Note that the type of each resource should only be printed once. What would be a reasonable type for the LocallyUnknownType and should it be declared? :-) (1 point)
+
 
 import rdflib
 import logging
 from rdflib import RDF, Graph, URIRef
-from pprint import pprint
 from rdflib.parser import Parser
+
+#We use this function to see if certain type is already familiar
+
+def addIfUnseenRes(r,s):
+  """Add s to r if so far unseen and of proper type (URIRef or BNode)"""
+  if not (type(s) is rdflib.URIRef or type(s) is rdflib.BNode): return
+  if not (s in r): r.append(s)
 
 # In order to get error messages straight from rdflib...
 logging.basicConfig()
 
 # Creating the graph with parser
 g = rdflib.Graph()
-g.open("store", create=True)
 result = g.parse("http://data.linked-open-science.org/semantic-dogfood/eswc-2013-complete.rdf", format='application/rdf+xml')
 
-#Go through the graph and place LocallyUnknownType if URIRef is unknown
+# Create a list of resource types
+r = []
 for s,p,o in g:
-    if not type(s) is rdflib.URIRef:
-        nt=(s,rdflib.RDFS.__class__LocallyUnknownType,o)
-        g.add(nt)
+    addIfUnseenRes(r,o)
 
 # Navigate through the triples
-for s, p, o in g.triples((None, RDF.type, None)) :
-    print("%s"%(o))
+for s,p,o in g.triples((None, RDF.type, None)) :
+    if (o in r):
+        print("%s"%(o))
